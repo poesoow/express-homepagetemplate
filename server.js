e0c4b0ca40e4373dc4f96985a425e823ba494480
peosoow <ws71144712@daum.net>
2023-06-09 16:47:47 +0900

    views 폴더 테스트

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const ObjectId = require('mongodb').ObjectId
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
 require("dotenv").config();
